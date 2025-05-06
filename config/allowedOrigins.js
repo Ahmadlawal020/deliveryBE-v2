c8ddb5e85bc0dd5919f2f07d14d31dadfde7190a
Ahmad Saulawa <ahmadlawal020@gmail.com>
2025-05-06 21:27:58 +0100

    merge resolve

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
   "http://ridedelivery.co.uk",
   "www.ridedelivery.co.uk",
   "ridedelivery.co.uk",
-  "https://moveitasp.netlify.app",
   "moveitasp.netlify.app",
   "www.moveitasp.netlify.app",
 ];
