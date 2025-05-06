d8a1ce4ad7e0c4b2121bdb24c1a2de53ee46d8f6
Ahmadlawal020 <ahmadlawal020@gmail.com>
2025-05-06 19:54:52 +0100

    Update allowedOrigins.js
    Update allowedOrigins.js v2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,10 @@
 const allowedOrigins = [
-  "https://moveitasp.netlify.app",
   "http://ridedelivery.co.uk",
   "www.ridedelivery.co.uk",
   "ridedelivery.co.uk",
-  "moveitasp.netlify.app",
-  "www.moveitasp.netlify.app",
+    "https://ridedelivery.netlify.app",
+  "ridedelivery.netlify.app",
+  "www.ridedelivery.netlify.app",
 ];
 
 module.exports = allowedOrigins;
